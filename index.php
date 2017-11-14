b0a59f626b6c9ffba92e8e2cde16079f0ac352d1
edmngit <33640541+edmngit@users.noreply.github.com>
2017-11-14 17:08:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,12 +18,9 @@ if ($getResults == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
  echo ($row['productid'] . " " . $row['name'].  PHP_EOL);
- 
+ echo(" ");
 }
-while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    echo ($row['productid'] . " " . $row['name'].  PHP_EOL); }
-    echo(" ");
-}
+
 sqlsrv_free_stmt($getResults);
 
 ?>
