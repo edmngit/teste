f2d1f2358aa98e3b59caa9cb82b86244bdf6b1f9
edmngit <33640541+edmngit@users.noreply.github.com>
2017-11-14 16:45:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,8 @@ echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
- echo ($row['productid']; 
- echo ($row['name'];
-       
+ echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
 }
 sqlsrv_free_stmt($getResults);
+
 ?>
