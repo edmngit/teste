d509e9d8457dbf1f0c25b8cf39fa5c2ef4e2b998
edmngit <33640541+edmngit@users.noreply.github.com>
2017-11-14 16:33:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 echo "Hello World!";
+
+$serverName = "sbr1.database.windows.net";
+$connectionOptions = array(
+    "Database" => "dbazure",
+    "Uid" => "adminbd",
+    "PWD" => "Arvorecaldo01."
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+
+
 ?>
