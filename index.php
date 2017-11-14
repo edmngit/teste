08b4251fd1f55ec167d6db72b2406af2049013f6
edmngit <33640541+edmngit@users.noreply.github.com>
2017-11-14 16:38:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -14,4 +14,10 @@ $tsql= "select top 100 productid,name from saleslt.Product";
 $getResults= sqlsrv_query($conn, $tsql);
 echo ("Reading data from table" . PHP_EOL);
 
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo ($row['productid'] . " " . $row['Name'] . PHP_EOL);
+}
+sqlsrv_free_stmt($getResults);
 ?>
