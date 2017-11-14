b7ddeda2079ebd098627b6cdb5983bb6c1a1d417
edmngit <33640541+edmngit@users.noreply.github.com>
2017-11-14 16:42:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $connectionOptions = array(
 );
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
-$tsql= "select top 100 productid,name from saleslt.Product1";
+$tsql= "select top 100 productid,name from saleslt.Product";
 
 $getResults= sqlsrv_query($conn, $tsql);
 echo ("Reading data from table" . PHP_EOL);
@@ -17,7 +17,9 @@ echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
- echo ($row['productid'] . " " . $row['Name'] . PHP_EOL);
+ echo ($row['productid']; 
+ echo ($row['name'];
+       
 }
 sqlsrv_free_stmt($getResults);
 ?>
